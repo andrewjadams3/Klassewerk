de790fa6c21e4c5db0f65d37d539d77eee9aa62c
Joe Webb <imjoewebb@gmail.com>
2014-08-14 11:36:13 -0500

    removed course_id from classrooms

--- db/migrate/20140814161553_create_classrooms.rb
+++ db/migrate/20140814161553_create_classrooms.rb
@@ -1,7 +1,6 @@
 class CreateClassrooms < ActiveRecord::Migration
   def change
     create_table :classrooms do |t|
-      t.belongs_to :course
       t.belongs_to :teacher
       t.string :title
       t.belongs_to :grading_period
