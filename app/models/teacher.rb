b6e46ffbc399f41f2aa94c009738d02244f3ec40
Adrian <modernamun@gmail.com>
2014-08-14 18:40:50 -0500

    adding has secure password to models for andrew to test

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,10 @@ class Teacher < ActiveRecord::Base
   belongs_to :school
   has_many :classrooms
   has_many :assignments, through: :classrooms
+
+  validates :first_name, :last_name, :username, :security_question, :security_answer,:password_digest, :email, presence: true
+
 end
+
+
+
--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -11,10 +11,42 @@ require 'rails_helper'
 #   end
 
 RSpec.describe School, :type => :model do
-   describe "instance variables" do
-    it 'should have a code' do
-      @school = School.create(:code => "CODE-1234")
-      expect(@school.code).to eq("CODE-1234") 
-    end
+   
+  
+ before do
+    @school = School.new(:code => "CODE-1234", :location => "Chi City", :mascot => "Wind", :name => "That Place")
   end
+
+  subject(:school) { @school }
+  it { is_expected.to respond_to(:code) }
+  it { is_expected.to respond_to(:location) }
+  it { is_expected.to respond_to(:mascot) }
+  it { is_expected.to respond_to(:name) }
+
+  it 'should have a code' do
+    expect(@school.code).to eq("CODE-1234")
+  end
+
+  it 'should have a name' do
+    expect(@school.name).to eq("That Place")
+  end
+
+  it 'should have a location' do
+    expect(@school.location).to eq("Chi City")
+  end
+
+  it 'should have a mascot' do
+    expect(@school.mascot).to eq("Wind")
+  end
+
+  it { is_expected.to be_valid }
+
+  
+  describe "Must have all validations" do
+    before do
+      @school = School.new(:code => "CODE-1234", :location => "Chi City", :mascot => "Wind")
+    end
+    it { is_expected.to_not be_valid }
+  end  
+
 end
