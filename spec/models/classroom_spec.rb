81a2f1f083bb7b75a4513e4faf822294f755937b
Joe Webb <imjoewebb@gmail.com>
2014-08-14 17:39:58 -0500

    Created validations and tests, left off on grading

--- spec/models/classroom_spec.rb
+++ spec/models/classroom_spec.rb
@@ -13,12 +13,39 @@ RSpec.describe Classroom, :type => :model do
   
   describe "associations" do
     before do
-      @classroom = Classroom.create
+      @classroom = Classroom.new(title: "Reading", )
+      @classroom.teacher = Teacher.new
+      @classroom.grading_period = GradingPeriod.new
     end
 
+    subject(:classroom) { @classroom }
+    it { is_expected.to respond_to(:title)}
+    it { is_expected.to respond_to(:teacher)}
+    it { is_expected.to respond_to(:grading_period)}
+
     it 'should belong to a teacher' do
-      @classroom.teacher = Teacher.create
       expect(@classroom.teacher).to be_a(Teacher)
+    end
+
+    it 'should belong to a grading_period' do
+      expect(@classroom.grading_period).to be_a(GradingPeriod)
+    end
+
+    it { is_expected.to be_valid }
+
+    describe "when a title is not present" do
+      before {@classroom.title = nil }
+      it { is_expected.to_not be_valid }
+    end
+
+    describe "when a grading_period is not present" do
+      before {@classroom.grading_period = nil }
+      it { is_expected.to_not be_valid }
+    end
+
+    describe "when a teacher is not present" do
+      before {@classroom.teacher = nil }
+      it { is_expected.to_not be_valid }
     end
   end
 end
