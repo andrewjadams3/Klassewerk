3fee92c5796d92790df396cb03664d0c37d4920d
Adrian <modernamun@gmail.com>
2014-08-16 12:29:35 -0500

    Adding name and code to worksheets and classrooms respectively

--- db/migrate/20140814161553_create_classrooms.rb
+++ db/migrate/20140814161553_create_classrooms.rb
@@ -1,6 +1,7 @@
 class CreateClassrooms < ActiveRecord::Migration
   def change
     create_table :classrooms do |t|
+      t.string :code
       t.belongs_to :teacher
       t.string :title
       t.belongs_to :grading_period
