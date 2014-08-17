23226f7c7023ffe900fc8d7d5b362f084a8edfbb
Spencer Zhang <syz4@cornell.edu>
2014-08-17 18:04:55 -0500

    Edited format

--- app/assets/javascripts/routes/worksheets.js
+++ app/assets/javascripts/routes/worksheets.js
@@ -1,13 +1,20 @@
 App.WorksheetsRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('worksheet');
+  },
+  renderTemplate: function() {
+    this.render('teacherapp/worksheets', {
+      into: "teacherapp",
+      outlet: 'left'
+    })
   }
 });
 
 App.WorksheetsNewRoute = Ember.Route.extend({
   renderTemplate: function() {
     this.render({
-      into: "worksheets"
+      into: "teacherapp",
+      outlet: "right"
     })
   }
 })
