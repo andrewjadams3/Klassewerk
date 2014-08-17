120d9b569428b2c7d9fe36f41fe6f1418cfb80fc
Andrew Adams <andrewjadams3@gmail.com>
2014-08-17 13:33:15 -0500

    Clean up old models, working on submissions

--- app/assets/javascripts/routes/assignments.js
+++
@@ -1,8 +0,0 @@
-App.AssignmentsRoute = Ember.Route.extend({
-  model: function(){
-    return this.store.find('assignment', {classroom: parseInt(this.modelFor('classroom').id)});
-  },
-  renderTemplate: function() {
-    this.render({ outlet: 'assignments' });
-  }
-})
