a0744e853218830ad8e3c3ca8398a07ad351998b
Andrew Adams <andrewjadams3@gmail.com>
2014-08-18 19:45:46 -0500

    Index pages for teacherapp

--- app/assets/javascripts/routes/teacherapp.js
+++ app/assets/javascripts/routes/teacherapp.js
@@ -5,3 +5,16 @@ App.TeacherappRoute = Ember.Route.extend({
     })
   }
 })
+
+App.TeacherappIndexRoute = Ember.Route.extend({
+  renderTemplate: function() {
+    this.render('teacherapp/index_left', {
+      into: 'teacherapp',
+      outlet: 'left'
+    })
+    this.render('teacherapp/index_right', {
+      into: 'teacherapp',
+      outlet: 'right'
+    })
+  }
+})
