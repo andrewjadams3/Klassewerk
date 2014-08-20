4c9201cb6a1a38de0f5ff98b2ebf9a5d5b8063c4
Joe Webb <imjoewebb@gmail.com>
2014-08-19 19:28:42 -0500

    updated some model tests

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,53 +1,37 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# # def change
-# #     create_table :teachers do |t|
-# #       t.string :first_name
-# #       t.string :last_name
-# #       t.string :username
-# #       t.string :security_question
-# #       t.string :security_answer
-# #       t.string :password_digest
-# #       t.string :email
-# #       t.belongs_to :school
-# #       t.timestamps
-# #     end
-
-# RSpec.describe Teacher, :type => :model do
-#   before do
-#     @teacher = Teacher.new(:first_name => "Joe", :last_name => "Webb", :password_digest => "Hello", :email => "email@email.com", :school => School.new)
-#     @teacher.save
+# create_table "teachers", force: true do |t|
+#     t.string   "first_name"
+#     t.string   "last_name"
+#     t.string   "email"
+#     t.string   "class_code"
+#     t.string   "password_digest"
+#     t.datetime "created_at"
+#     t.datetime "updated_at"
 #   end
 
-#   it 'should belong to a school' do
-#     expect(@teacher.school).to be_a(School)
-#   end
+RSpec.describe Teacher, :type => :model do
+  before do
+    @teacher = Teacher.new(
+      :first_name => "Bill", 
+      :last_name => "Teacher", 
+      :password => "password", 
+      :email => "email@email.com", 
+      :class_code => "123-456")
+    @teacher.save
+  end
 
-#   subject(:teacher) { @teacher }
-#   it { is_expected.to respond_to(:first_name) }
-#   it { is_expected.to respond_to(:last_name) }
-#   it { is_expected.to respond_to(:password_digest) }
-#   it { is_expected.to respond_to(:email) }
-#   it { is_expected.to be_valid }
+  subject(:teacher) { @teacher }
+  it { is_expected.to respond_to(:first_name) }
+  it { is_expected.to respond_to(:last_name) }
+  it { is_expected.to respond_to(:password_digest) }
+  it { is_expected.to respond_to(:class_code) }
+  it { is_expected.to respond_to(:email) }
+  
+  it { is_expected.to be_valid }
 
-#   it 'should have a first_name' do
-#     expect(@teacher.first_name).to eq("Joe")
-#   end
+  it "has no errors" do
+    expect(@teacher.errors.messages).to eq({})
+  end
 
-#   it 'should have a last_name' do
-#     expect(@teacher.last_name).to eq("Webb")
-#   end
-
-#   it "has no errors" do
-#     expect(@teacher.errors.messages).to eq({})
-#   end
-
-# end
-
-
-
-
-
-
-
-
+end
