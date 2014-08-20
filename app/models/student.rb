1372023b8dbd19bcaa3d779189002d9d0326a4c8
Joe Webb <imjoewebb@gmail.com>
2014-08-19 19:28:57 -0500

    added validations to student and teacher

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,9 +2,13 @@ class Student < ActiveRecord::Base
   belongs_to :teacher
   has_many :responses
 
-  validates :first_name, :last_name, :password_digest, :teacher, presence: true
+  validates :first_name, :last_name, :username, :password, :password_confirmation, :teacher, presence: true
   validates :username, presence: true, uniqueness: true
 
+  before_validation :downcase_fields
   has_secure_password
 
+  def downcase_fields
+    self.username = self.username.downcase
+  end
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,11 +3,16 @@ class Teacher < ActiveRecord::Base
   has_many :worksheets
   has_many :responses, through: :students
 
-  validates :first_name, :last_name, :password_digest, presence: true
+  validates :first_name, :last_name, :password, :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
   has_secure_password
 
+  before_validation :downcase_fields
+  def downcase_fields
+    self.email = self.email.downcase
+  end
 end
 
 
